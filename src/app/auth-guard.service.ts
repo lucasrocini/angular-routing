1f63de18d8f7ddb06e62ce0d6777f392f9f5b425
Lucas <lucasrocini@hotmail.com>
2021-09-05 23:09:29 -0300

    Routing, Query Parameters, Guards, Passing Data, caDeactivate() and more

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,36 @@
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from "@angular/router";
+import { Observable } from "rxjs";
+
+import { Injectable } from "@angular/core";
+import { AuthService } from "./auth.service";
+@Injectable({
+    providedIn: 'root'
+  })
+
+
+export class AuthGuard implements CanActivate, CanActivateChild {
+
+    constructor(private authService: AuthService, 
+                private router: Router) {}
+
+    canActivateChild(route: ActivatedRouteSnapshot, 
+        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+        return this.canActivate(route, state);
+    }
+
+    canActivate(route: ActivatedRouteSnapshot, 
+        state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+            return this.authService.isAuthenticated()
+                .then(
+                    (authenticated: boolean ) => {
+                        if(authenticated) {
+                            return true;
+                        } else {
+                            this.router.navigate(['/']);
+                        }
+                    }
+                );
+    }
+
+}
